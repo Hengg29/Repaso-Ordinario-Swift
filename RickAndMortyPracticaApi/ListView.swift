0289e807af8c1791a9c5721963671b0e56d90625
Alexander Hernandez Henggeler <alexander.hernandez@iest.edu.mx>
2025-11-30 17:22:21 -0600

    Cambiar de color la lista

--- RickAndMortyPracticaApi/ListView.swift
+++ RickAndMortyPracticaApi/ListView.swift
@@ -8,19 +8,37 @@
 import SwiftUI
 
 struct ListView: View {
-@StateObject var RickList = RickListViewModel()
+    @StateObject var RickList = RickListViewModel()
     var body: some View {
-        Text("\(RickList.personajes.count)")
-      
-        List(RickList.personajes) { personaje in
-            Text(personaje.name)
-            AsyncImage(url: URL(string: personaje.image)!)
-        }
-        .onAppear {
-            RickList.getPersonajes()
+        ZStack{
+            Color.black //Cambia de color lo que esta afuera de la lista
+                .ignoresSafeArea()
+            
+            List(RickList.personajes) { personaje in
+                HStack(spacing: 16) {
+                    AsyncImage(url: URL(string: personaje.image)) { img in
+                        img.resizable()
+                    } placeholder: {
+                        ProgressView()
+                    }
+                    .frame(width: 80, height: 80)
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+
+                    Text(personaje.name)
+                        .foregroundColor(.white)
+                        .font(.headline)
+                }
+                .padding(.vertical, 8)
+                .listRowBackground(Color.gray)//cambia la lista de color
+            }
+            .scrollContentBackground(.hidden)
+            .onAppear {
+                RickList.getPersonajes()
+            }
         }
     }
 }
+
 
 #Preview {
     ListView()
