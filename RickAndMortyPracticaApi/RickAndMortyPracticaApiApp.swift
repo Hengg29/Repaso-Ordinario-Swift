b669804cc5967e1c4a66d04de50536f7cf9d5f29
Alexander Hernandez Henggeler <alexander.hernandez@iest.edu.mx>
2025-11-30 11:08:49 -0600

    Initial Commit

---
+++ RickAndMortyPracticaApi/RickAndMortyPracticaApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyPracticaApiApp.swift
+//  RickAndMortyPracticaApi
+//
+//  Created by Alexander Hernandez Henggeler on 30/11/25.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyPracticaApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
